da6276f97459d91830311ac778c4d659e3bda474
ls <izqiaulhassan166@gmail.com>
2026-07-31 19:21:06 +0500

    started working on backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import db from "./models/index.js";
+const { sequelize } = db;
 
 
 dotenv.config();
@@ -15,6 +17,8 @@ app.use(express.json());
 app.get("/api/health" , (req, res)=>{
     req.json({status:"ok" , messgage : "backend is running"})
 })
+
+sequelize.authenticate().then(()=> console.log("postgres connected via sequelize")).catch(err=> console.log("unable to connect" ,err))
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
